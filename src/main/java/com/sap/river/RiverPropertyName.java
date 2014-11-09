f0ef855ee228d52af09145bb52411a92d08f3e8a
Asaf Bruner <asaf.bruner@sap.com>
2014-11-09 17:09:42 +0200

    A test for the new gerrit

--- src/main/java/com/sap/river/RiverPropertyName.java
+++ src/main/java/com/sap/river/RiverPropertyName.java
@@ -9,25 +9,23 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  * @since 1.1.1
  */
 public enum RiverPropertyName {
-    AUSTRALIA("Australia"),
-    UNITED_STATES("United States"),
-    GERMANY("Germany"),
-    NOT_SPECIFIED("None of your business!");
-    
-    private String propertyName;
-    
-    private RiverPropertyName(String propertyName) {
-        Validate.notBlank(propertyName, "Property name required");
-        this.propertyName = propertyName;
-    }
-    
-    public String getPropertyName() {
-        return propertyName;
-    }
+	AUSTRALIA("Australia"), UNITED_STATES("United States"), GERMANY("Germany"), ISRAEL("Israel"), NOT_SPECIFIED("None of your business!");
 
-    public String toString() {
-        final ToStringBuilder builder = new ToStringBuilder(this);
-         builder.append("propertyName", propertyName);
-        return builder.toString();
-    }
+	private String propertyName;
+
+	private RiverPropertyName(String propertyName) {
+		Validate.notBlank(propertyName, "Property name required");
+		this.propertyName = propertyName;
+	}
+
+	public String getPropertyName() {
+		return propertyName;
+	}
+
+	@Override
+	public String toString() {
+		final ToStringBuilder builder = new ToStringBuilder(this);
+		builder.append("propertyName", propertyName);
+		return builder.toString();
+	}
 }
