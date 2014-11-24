0cecf0a87818513c7480e146d436d220b8b85260
fonio <fonio@hotmail.com>
2014-11-24 16:53:44 +0200

    Fix resources node planting to be safer and to consider edge cases
    Change-Id: I18facdb5a2a96cceada40584232a65e18ef12afa

--- src/main/java/com/sap/river/hcp/WebAppOperationsImpl.java
+++ src/main/java/com/sap/river/hcp/WebAppOperationsImpl.java
@@ -3,9 +3,9 @@ package com.sap.river.hcp;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.logging.Logger;
 
-import javax.print.DocFlavor.INPUT_STREAM;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.Validate;
@@ -21,6 +21,7 @@ import org.springframework.roo.support.util.FileUtils;
 import org.springframework.roo.support.util.XmlUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 @Component
 @Service
@@ -31,7 +32,20 @@ public class WebAppOperationsImpl implements WebAppOperations {
 
 	private static final String WEB_MVC_XML = "WEB-INF/spring/webmvc-config.xml";
 	private static final String VIEW_XML = "WEB-INF/views/views.xml";
+	private static final String LOCATION_ATTR = "location";
+	private static final String MAPPING_ATTR = "mapping";
+	private static final String LOCATION_ATTR_VALUE = "/, classpath:/META-INF/web-ui5/";
+	private static final String MAPPING_ATTR_VALUE = "/**";
+	private static final String MVC_RESOURCE = "mvc:resources";
+	private static final String BEANS_XPATH = "/beans";
+	private static final String RESOURCES_XPATH = "/beans/resources";
+	private static final String ERROR_MSG_XML_NOT_CREATED_FORMAT = "%s has not yet been generated. Make sure to run add-on again.";
+	private static final String ERROR_MSG_LOCATION_CONFLICT = "Confilict in webmvc-config.xml. Resource in location " + LOCATION_ATTR_VALUE  + " is already mapped to a different value.";
+	private static final String ERROR_MSG_BEANS_NODE_NOT_FOUND = "beans not found.";
+
+
 	/** river specific details in the project setup files (such as pom.xml etc.) */
+
 	@Reference
 	private ProjectOperations projectOperations;
 	@Reference
@@ -89,7 +103,8 @@ replace the exsiting tag with this one:
 */
 	private void setupViewsXml() 
 	{
-        final String viewsPath = pathResolver.getFocusedIdentifier(Path.SRC_MAIN_WEBAPP, VIEW_XML); 
+// TODO replace the attribute in a more elegant way
+		final String viewsPath = pathResolver.getFocusedIdentifier(Path.SRC_MAIN_WEBAPP, VIEW_XML); 
         final String old_value = "/WEB-INF/views/index.jspx";
         final String new_value = "index.html";
         InputStream is = fileManager.getInputStream(viewsPath);
@@ -105,23 +120,99 @@ replace the exsiting tag with this one:
         fileManager.createOrUpdateTextFileIfRequired(viewsPath, allClassContent, true);
 	}
 
-	/**
-	 1) in webmvc-config.xml add the line:
+/**
+  	Set the path to the ui5 library.
+  	In webmvc-config.xml add the line:
 	<mvc:resources location="/, classpath:/META-INF/web-ui5/" mapping="/**"/>
-	*/
+	
+	Make sure it is after any other <resources> node.
+	If no other <resources> node exists, append to the end of the <beans> node.
+*/
 	private void setupWebmvcConfigXml() 
 	{
         final String webMvcXMLPath = pathResolver.getFocusedIdentifier(Path.SRC_MAIN_WEBAPP, WEB_MVC_XML);
-        Validate.isTrue(fileManager.exists(webMvcXMLPath), webMvcXMLPath + " has not yet been generated. Make sure to run add-on again.");
+        Validate.isTrue(fileManager.exists(webMvcXMLPath), String.format(ERROR_MSG_XML_NOT_CREATED_FORMAT, webMvcXMLPath));
+
         final Document webXMLDoc = XmlUtils.readXml(fileManager.getInputStream(webMvcXMLPath));
         final Element root = webXMLDoc.getDocumentElement();
-        final Element webAppElement = XmlUtils.findFirstElement("/beans", root);
-        Validate.isTrue(webAppElement!=null, "beans not found.");
-        Element resourceRef = webXMLDoc.createElement("mvc:resources");
-        resourceRef.setAttribute("location", "/, classpath:/META-INF/web-ui5/");
-        resourceRef.setAttribute("mapping", "/**");
-        webAppElement.appendChild(resourceRef);
-        fileManager.createOrUpdateTextFileIfRequired(webMvcXMLPath, XmlUtils.nodeToString(webXMLDoc), false);
+        final Element beansElement = XmlUtils.findFirstElement(BEANS_XPATH, root);
+        Validate.isTrue(beansElement!=null, ERROR_MSG_BEANS_NODE_NOT_FOUND);
+
+        final List<Element> resources = XmlUtils.findElements(RESOURCES_XPATH, root);
+
+// check current resources under <beans>- 
+// if new node to be added exist- do nothing
+// if conflict - throw
+// else - call utility method to plant the node in the right place.
+        boolean needToAddResource = checkResourcesList(resources);
+
+        if (needToAddResource) {
+        	addNewResourcesNode(webXMLDoc, beansElement, resources);
+            fileManager.createOrUpdateTextFileIfRequired(webMvcXMLPath, XmlUtils.nodeToString(webXMLDoc), false);
+        }
+	}
+	
+/**
+ * Validate the resources nodes under beans
+ * 
+ * - If the addon's location exists, and mapping is the same - return false - no need to add a node
+ * - If the addon's location exists, and mapping is different - break with a proper message. We don't want to override existing nodes.
+ * - Else - return true indicating a node should be inserted 
+ * 
+ * @param resources - list of the resources under <beans> node 
+ * @return true is new node needs to be inserted
+ * @throws if resources have a conflict with the target location
+ */
+	private boolean checkResourcesList(List<Element> resources) 
+	{
+        if (resources != null && !(resources.isEmpty())) {
+	        for (final Element resourcesElement : resources) {
+                if (resourcesElement.getAttribute(LOCATION_ATTR).equals(LOCATION_ATTR_VALUE)) {
+                	Validate.isTrue(resourcesElement.getAttribute(MAPPING_ATTR).equals(MAPPING_ATTR_VALUE), ERROR_MSG_LOCATION_CONFLICT);
+                	return false;
+                }
+            }
+        }
+        return true;
+	}
+	
+/**
+ * Add the new resources node to the right place
+ * Assume there are no conflicts
+ * If other resources nodes exist - append the new node to the last one.
+ * Else - append as the last child of the beans node
+ * @param webXMLDoc
+ * @param beansElement
+ * @param resources
+ */
+	private void addNewResourcesNode(Document webXMLDoc, Element beansElement, List<Element> resources) 
+	{
+		/**
+		 * Insert the new node after the last <resources> node found.
+		 * Unfortunately, There is no insertAfter utility, so we:
+		 * 
+		 * - Clone the last resource node - Insert the clone before the original
+		 * - Update the original nodes attributes to "our" values
+		 * 
+		 */
+		if (resources != null && !(resources.isEmpty())) {
+			Element lastResourceElement = resources.get(resources.size() - 1);
+			Node resourceRefNode = lastResourceElement.cloneNode(false);
+			beansElement.insertBefore(resourceRefNode, lastResourceElement);
+			lastResourceElement
+					.setAttribute(LOCATION_ATTR, LOCATION_ATTR_VALUE);
+			lastResourceElement.setAttribute(MAPPING_ATTR, MAPPING_ATTR_VALUE);
+		}
+		/**
+		 * There are no resources node - append the node as the last child of
+		 * <beans>
+		 */
+		else {
+			Element resourceRef = webXMLDoc.createElement(MVC_RESOURCE);
+			resourceRef.setAttribute(LOCATION_ATTR, LOCATION_ATTR_VALUE);
+			resourceRef.setAttribute(MAPPING_ATTR, MAPPING_ATTR_VALUE);
+			beansElement.appendChild(resourceRef);
+		}
 	}
 }
 
