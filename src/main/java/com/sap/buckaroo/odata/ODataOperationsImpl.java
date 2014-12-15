242ce7186da7da23954f4204c5c648e0690d1979
fonio <fonio@hotmail.com>
2014-12-15 16:31:37 +0200

    Support logs Enable slf4j Enable log4j for tests Add hidden commands odata setup-test && setup-ext to generate odata test function tests setup-test: update generated code setup-ext: setup + test
    Change-Id: I83eed714215b41762a74d66ea8111fa153f15bb7

--- src/main/java/com/sap/buckaroo/odata/ODataOperationsImpl.java
+++ src/main/java/com/sap/buckaroo/odata/ODataOperationsImpl.java
@@ -629,6 +629,16 @@ public class ODataOperationsImpl implements ODataOperations {
 	 * @param factoryClassName - the name of the factory class
 	 */
 	protected void createJPAServiceFactoryClass(JavaType factoryClass) {
+		createJPAServiceFactoryClass(factoryClass, false);
+	}
+
+	/**
+	 * Creates the generated OData service factory class
+	 * 
+	 * @param factoryClassName - the name of the factory class
+	 * @param withTestExt - flag that indicates whether to load test extension 
+	 */
+	protected void createJPAServiceFactoryClass(JavaType factoryClass, boolean  withTestExt) {
 
 		final String declaredByMetadataId = PhysicalTypeIdentifier.createIdentifier(factoryClass,
 				pathResolver.getFocusedPath(Path.SRC_MAIN_JAVA));
@@ -649,7 +659,9 @@ public class ODataOperationsImpl implements ODataOperations {
 		String newContents = typeParsingService.getCompilationUnitContents(cid);
 
 		// Adding rest of the imports
-		final InputStream inputStream = FileUtils.getInputStream(DeployCommands.class, "JPAServiceFactory.txt");
+		final String templateFile = (withTestExt) ? "JPAServiceFactoryWTesterExtention.txt" : "JPAServiceFactory.txt";
+		final InputStream inputStream = FileUtils.getInputStream(DeployCommands.class, templateFile);
+
 		String allClassContent = "";
 		try {
 			allClassContent = IOUtils.toString(inputStream);
@@ -935,5 +947,21 @@ public class ODataOperationsImpl implements ODataOperations {
 				+ "System.out.println((new StringBuilder(\"Entry: \")).append(entry.getMetadata().getUri()).toString());\n" + "}\n");
 		methodBuilder.setBodyBuilder(bodyBuilder);
 		return methodBuilder;
+	}
+
+/**
+ * Currently check for a hidden file only We know about.
+ * TODO - search for system property
+ */
+	@Override
+	public boolean isSetupTesterExtentionAvailable() {
+		return fileManager.exists("C:\\Temp\\.ROO_internal");
+	}
+
+	@Override
+	public void setupTesterExtention(final JavaType factoryClassName) {
+		createClassFromFile("TesterProcessingExtension.txt", "TesterProcessingExtension");
+		createClassFromFile("TesterProcessor.txt", "TesterProcessor");
+		createJPAServiceFactoryClass(factoryClassName, true);
 	}
 }
